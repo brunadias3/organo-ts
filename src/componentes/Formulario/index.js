fbb169df9dcc1e2551771fc6184fcde676b80480
Bruna <brunadias_30@hotmail.com>
2023-01-28 19:59:39 -0300

    campoTexto tsx

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Botao from '../Botao'
-import CampoTexto from '../CampoTexto/CampoTexto.js'
+import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
